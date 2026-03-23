12fa2d674b541517d68a84a5952294ee004616e0
tmdzm <lvmoon10@gmail.com>
2026-03-23 17:46:18 +0900

    feat:20260323 aws account creation, ec2, elastic ip

---
+++ aws-ec2-springboot-main/src/main/java/com/example/awsec2springboot/AwsEc2SpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.awsec2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2SpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsEc2SpringbootApplication.class, args);
+  }
+}
---
+++ aws-ec2-springboot-main/src/test/java/com/example/awsec2springboot/AwsEc2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsec2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEc2SpringbootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
